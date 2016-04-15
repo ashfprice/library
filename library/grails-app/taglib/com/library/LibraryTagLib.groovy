a8e995f24b3052977494716ddb5772b22e9d9b61
ashfprice <b4035277@my.shu.ac.uk>
2016-04-15 15:50:14 +0100

    The homepage of the app has been edited with a new title and have a login setup

---
+++ library/grails-app/taglib/com/library/LibraryTagLib.groovy
@@ -0,0 +1,32 @@
+package com.library
+
+class LibraryTagLib {
+    static defaultEncodeAs = [taglib:'html']
+    //static encodeAsForTags = [tagName: [taglib:'html'], otherTagName: [taglib:'none']]
+
+def loginToggle={
+
+out<<"<div style='marigin: 15px 0 40px;'>"
+
+if(request.getSession(false) && session.user){
+
+out<<"<div style='marigin: 15px 0 40px;'>"
+
+out<<"<span style='float:left;margin-left:15px'>"
+out<<"Welcome${session.user}."
+out<<"</span><span style='float:right;margin-right:15px'>"
+out<<"<a href='${createLink(controller:'librarian',action:'logout')}'>"
+out<<"Logout</a></span>"
+
+}else{
+
+out<<"<span style='float:right;margin-right:10px'>"
+out<<"<a href='${createLink(controller:'librarian',action:'login')}'>"
+out<<"Login</a></span>"
+}
+
+out<<"</div><br/>"
+
+}
+}
+}
